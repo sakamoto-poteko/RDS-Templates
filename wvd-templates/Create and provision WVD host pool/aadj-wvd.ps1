96f770df25b9aa6f7a0e62be3a506e00d8f0ca1f
Fangning Cheng <fac@microsoft.com>
2020-10-22 17:51:02 +0800

    wvd enroll

--- wvd-templates/Create and provision WVD host pool/aadj-wvd.ps1
+++ wvd-templates/Create and provision WVD host pool/aadj-wvd.ps1
@@ -13,10 +13,10 @@ Invoke-WebRequest "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWr
 Write-Host "files downloaded"
 
 Write-Host "installing rd agent"
-Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i C:\wvd\rdagent.msi /qn" -Wait
+Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/l*v C:\wvd\rda_log.log /i C:\wvd\rdagent.msi /qn REGISTRATIONTOKEN=$RegistrationToken" -Wait
 
 Write-Host "installing rd bootloader"
-Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i C:\wvd\rdbootloader.msi /qn" -Wait
+Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList '/l*v C:\wvd\rdbl_log.log /i C:\wvd\rdbootloader.msi /qn' -Wait
 
 Stop-Service -Name "RdAgent" -Force
 Stop-Service -Name "RDAgentBootLoader" -Force
@@ -24,15 +24,8 @@ Stop-Service -Name "RDAgentBootLoader" -Force
 Write-Host "enabling rd aadj"
 New-Item -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent' -Name 'AADJPrivate' -Force
 
-Write-Host "setting rd registration token"
-Set-ItemProperty -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent' -Name 'RegistrationToken' -Value $RegistrationToken -Force
-
 Write-Host "restarting rd agent"
 Restart-Service -Name "RdAgent" -Force
 Restart-Service -Name "RDAgentBootLoader" -Force
-
-Write-Host "disabling NLA"
-(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").UserAuthenticationRequired
-(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
 
 Write-Host "you got a new Cloud PC!"
