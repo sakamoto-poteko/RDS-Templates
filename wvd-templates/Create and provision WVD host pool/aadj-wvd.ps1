04d03ec143748dcb7b19a9465cc2a1d89a45fb3c
Fangning Cheng <fac@microsoft.com>
2020-10-22 13:20:30 +0800

    ps changed

--- wvd-templates/Create and provision WVD host pool/aadj-wvd.ps1
+++ wvd-templates/Create and provision WVD host pool/aadj-wvd.ps1
@@ -18,6 +18,9 @@ Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i C:\w
 Write-Host "installing rd bootloader"
 Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i C:\wvd\rdbootloader.msi /qn" -Wait
 
+Stop-Service -Name "RdAgent" -Force
+Stop-Service -Name "RDAgentBootLoader" -Force
+
 Write-Host "enabling rd aadj"
 New-Item -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent' -Name 'AADJPrivate' -Force
 
@@ -25,7 +28,8 @@ Write-Host "setting rd registration token"
 Set-ItemProperty -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\RDInfraAgent' -Name 'RegistrationToken' -Value $RegistrationToken -Force
 
 Write-Host "restarting rd agent"
-Restart-Service -Name "RDAgentBootLoader"
+Restart-Service -Name "RdAgent" -Force
+Restart-Service -Name "RDAgentBootLoader" -Force
 
 Write-Host "disabling NLA"
 (Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").UserAuthenticationRequired
